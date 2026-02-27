6688ba1269c48417486ba9ff1be75aefebe02d0f
RajatKhandelwal <rajatkhandelwal2310@gmail.com>
2026-02-27 11:34:37 +0530

    SetMatrixZero

---
+++ SetMatrixZero.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0=1;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int row=0; row<m; row++){
+            for(int col=0; col<n; col++){
+                if(matrix[row][col]==0){
+                    matrix[row][0]=0;
+                    if(col!=0){
+                        matrix[0][col]=0;
+                    }else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+        for(int row=1; row<m; row++){
+            for(int col=1; col<n; col++){
+                if(matrix[row][0]==0 || matrix[0][col]==0){
+                    if(matrix[row][col]!=0){
+                        matrix[row][col]=0;
+                    }
+                }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int col=0; col<n; col++){
+                matrix[0][col]=0;
+            }
+        }
+        if(col0==0){
+            for(int row=0; row<m; row++){
+                matrix[row][0]=0;
+            }
+        }
+    }
+};
